872a2de937b47b83b48f2eb9ca20540575b9f887
Dhiraj Kumar <kumardhirajkumar908@gmail.com>
2024-04-09 16:15:59 +0530

    FastAPI Note Book

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-MONGO_URI="mongodb+srv://dhirajkmar2:lSSVrNFXxMYTIqKP@dk-cluster.jnct1tu.mongodb.net"
+MONGO_URI="mongodb+srv://dhirajkmar2:passwordP@dk-cluster.jnct1tu.mongodb.net"
 
 conn=MongoClient(MONGO_URI)
